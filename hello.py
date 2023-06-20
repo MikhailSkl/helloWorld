ea3593d7d572f6db5883574d8374fc3baf3d71a6
MikhailSkl <mikhailskalyga@gmail.com>
2023-06-20 13:35:28 +0200

    Added geatures

--- hello.py
+++ hello.py
@@ -6,8 +6,10 @@ def func(a):
     """
     Test debugging
     """
-
-    print(a)
+    if a == 3:
+        return None
+    else:
+        print(a)
 
     return None
 b_df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
@@ -15,3 +17,4 @@ b = 2
 func("Second print")
 func(b)
 func(b_df)
+func(b=3)
